7d2b2a3709150af1eb2569e12041af54f015872d
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-18 13:04:08 -0800

    grabbed dom elements, set pre-determined states, began ghost array in state

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 /* Imports */
 
 /* Get DOM Elements */
+const ghostsEl = document.querySelector('.ghosts');
+const ghostInputEl = document.getElementById('ghost-input');
+const ghostButtonEl = document.getElementById('ghost-button');
+const playerLivesEl = document.getElementById('player-lives');
+const ghostCountEl = document.getElementById('ghost-count');
 
 /* State */
+let livesCount = 3;
+let playerLives = 10;
 
+const ghosts = [
+    {
+        name: 'Lock',
+        lives: 3,
+    },
+    {
+        name: 'Shock',
+        lives: 2,
+    },
+    {
+        name: 'Barrel',
+        lives: 1,
+    },
+];
 /* Events */
 
 /* Display Functions */
