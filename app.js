365b679ee7beea9e6d9dd17cc0aa2aa52814876d
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-18 17:07:24 -0800

    added ghost bttn event listener

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /* Imports */
 
 /* Get DOM Elements */
-const ghostsEl = document.querySelector('.ghosts');
+const ghostsEl = document.getElementById('ghosts');
 const ghostInputEl = document.getElementById('ghost-input');
-const ghostButtonEl = document.getElementById('ghost-button');
+const ghostBttnEl = document.getElementById('ghost-bttn');
 const playerLivesEl = document.getElementById('player-lives');
-const ghostCountEl = document.getElementById('ghost-count');
+const caughtCountEl = document.getElementById('caught-count');
 
 /* State */
-let livesCount = 3;
+let ghostsCaught = 0;
 let playerLives = 10;
 
 const ghosts = [
@@ -27,6 +27,51 @@ const ghosts = [
 ];
 /* Events */
 
-/* Display Functions */
+//ghost bttn 'click'
+ghostBttnEl.addEventListener('click', () => {
+    console.log('addign name');
+    const ghostName = ghostInputEl.value; 
+    const newGhostEl = {
+        name: ghostName || `Dearly Departed ${Math.floor(Math.random() * 100)}`,
+        lives: 3, //ask about lives
+    };
+//push to array
+    ghostsEl.push(newGhostEl);
+    ghostInputEl.value = '';
+//call function to display, do next!
+    displayGhosts();
+});
 
+// newGhostEl 'click' -- inside display function??
+// newGhostEl.addEventListener('click', () => {
+
+// });
+
+/* Display Functions */
+function displayGhosts(){
+ghostsEl.textContent = '';
+
+for (let ghost of ghosts)
+
+};
 // (don't forget to call any display functions you want to run on page load!)
+
+// newFriendEl.addEventListener('click', () => {
+//     if (friend.satisfaction < 3 && mushroomCount > 0) {
+//         //and if the friend's satisfaction level is below 3 and you have mushrooms left DONE
+//         friend.satisfaction++;
+//         mushroomCount--;
+//         //increment the friends satisfaction and decrement your mushrooms DONE
+//         //clear out and display the updated friends and mushrooms (hint: displayFriends, displayMushrooms)
+//         displayFriends();
+//         displayMushrooms();
+
+//         //alerts to go forage for more or person is full
+//     } else if (friend.satisfaction === 3) {
+//         alert('All full! Give mushrooms to someone else!');
+//         return;
+//     } else if (mushroomCount === 0) {
+//         alert('Go forage for another mushroom!');
+//         return;
+//     }
+// });
